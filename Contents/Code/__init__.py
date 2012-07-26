e6ebdba21665af78828b74c47eaa51711eb80a01
Mark Guertin <mark@brainwebsolutions.com>
2012-07-26 14:46:08 -0400

    updates to use only webvideourl

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,7 +1,4 @@
-import datetime, operator
-
 ####################################################################################################
-
 TITLE = "Showcase"
 ART = 'art-default.jpg'
 ICON = 'icon-default.png'
@@ -9,14 +6,11 @@ SHOWCASE_PARAMS = ["sx9rVurvXUY4nOXBoB2_AdD1BionOoPy", "z/Showcase%20Video%20Cen
 FEED_LIST = "http://feeds.theplatform.com/ps/JSON/PortalService/2.2/getCategoryList?PID=%s&startIndex=1&endIndex=500&query=hasReleases&query=CustomText|PlayerTag|%s&field=airdate&field=fullTitle&field=author&field=description&field=PID&field=thumbnailURL&field=title&contentCustomField=title&field=ID&field=parent"
 FEEDS_LIST = "http://feeds.theplatform.com/ps/JSON/PortalService/2.2/getReleaseList?field=ID&field=contentID&field=PID&field=URL&field=airdate&PID=%s&contentCustomField=Episode&contentCustomField=Season&query=CategoryIDs|%s&field=thumbnailURL&field=title&field=length&field=description&startIndex=1&endIndex=500&sortField=airdate&sortDescending=true"
 DIRECT_FEED = "http://release.theplatform.com/content.select?format=SMIL&pid=%s&UserName=Unknown&Embedded=True"
-VIDEO_URL = 'http://www.%s/%s/video/full+episodes/%s/video.html?v=%s&p=1&s=dd#%s/video/full+episodes'
 LOADCATS = { 
 	'shows':['/Shows/']
 	}
 
-
 ###################################################################################################
-
 def Start():
 	Plugin.AddPrefixHandler("/video/showcase", MainMenu, TITLE, ICON, ART)
 
@@ -45,18 +39,15 @@ def MainMenu():
 def LoadShowList(cats):
 
  	oc = ObjectContainer ( view_group="List")
-	Log("started MainMenu")
 	network = SHOWCASE_PARAMS
 
 	content = JSON.ObjectFromURL(FEED_LIST % (network[0], network[1]))
 	showList = {}
 	showCount = 0
-	Log(content)
 	
 	items = content['items']
 
 	for item in items:
-		#Log("Gerk: fullTitle: %s",item['fullTitle'])
 		if WantedCats(item['fullTitle'],cats):
 			title = item['fullTitle'].split('/')[2]
 			iid = item['ID']
@@ -75,22 +66,8 @@ def LoadShowList(cats):
 
  	return oc
 
-####################################################################################################
-
-def VideoPlayer(sender, pid, show, title, id):
-
-	network = "showcase.ca"
-	show = show.replace(' ', '')
-
-	title = String.Quote(title, usePlus=True)
-
-	video_url = VIDEO_URL % (network, show, title, id, show)
-	#Log(video_url)
-
-	return Redirect(WebVideoItem(video_url))
 
 ####################################################################################################
-
 def VideosPage(pid, iid, show):
 	
 	oc = ObjectContainer(
@@ -100,10 +77,8 @@ def VideosPage(pid, iid, show):
 	feeds = JSON.ObjectFromURL(pageURL)
 
 	showList = {}
-	Log("Gerk: allFeeds: %s",feeds['items'])
 	
 	for item in feeds['items']:
-		#Log("Gerk: VideosPage item: %s",item)
 		title = item['title']
 		try:
 			# show exists, skip adding multiples
@@ -112,8 +87,10 @@ def VideosPage(pid, iid, show):
 		except:
 			# show doesn't exist, add it
 			showList[title]=""
-			pid = item['PID']
-			iid = item['contentID']
+			pid = str(item['PID'])
+			iid = str(item['contentID'])
+			url = "http://www.showcase.ca/video/video.html?v="+iid+"#video"
+			Log.Debug(url)
 			summary =  item['description']
 			duration = item['length']
 			thumb_url = item['thumbnailURL']
@@ -122,16 +99,13 @@ def VideosPage(pid, iid, show):
 			
 			try:
 				# try to set the seasons and episode info
-				# NB: episode is set with 'index' (not in framework docs)!
 				season = item['contentCustomData'][1]['value']
 				seasonint = int(float(season))
 				episode = item['contentCustomData'][0]['value']
 				episodeint = int(float(episode))
-	 			Log("Gerk: Seasoninfo %i-%i : %s",seasonint, episodeint, title)
 				oc.add(
 					EpisodeObject(
-						key = Callback(VideoParse, pid=pid, show=show, title=title, iid=iid),
-						rating_key = pid, 
+						url = url,
 						title = title,
 						summary=summary,
 						duration=duration,
@@ -146,8 +120,7 @@ def VideosPage(pid, iid, show):
 				# if we don't get the season/episode info then don't set it
 				oc.add(
 					EpisodeObject(
-						key = Callback(VideoParse, pid=pid, show=show, title=title, iid=iid),
-						rating_key = pid, 
+						url = url,
 						title = title,
 						summary=summary,
 						duration=duration,
@@ -158,26 +131,6 @@ def VideosPage(pid, iid, show):
 
 	return oc
 
-# def VideosPage(sender, pid, id, network=None, show=None):
-# 
-# 	dir = MediaContainer(title2=sender.itemTitle, viewGroup="InfoList", art=sender.art)
-# 	pageUrl = FEEDS_LIST % (pid, id)
-# 	feeds = JSON.ObjectFromURL(pageUrl)
-# 	#Log(feeds)
-# 
-# 	for item in feeds['items']:
-# 		title = item['title']
-# 		pid = item['PID']
-# 		summary =  item['description'].replace('In Full:', '')
-# 		duration = item['length']
-# 		thumb = item['thumbnailURL']
-# 		id = item['contentID']
-# 		airdate = int(item['airdate'])/1000
-# 		subtitle = 'Originally Aired: ' + datetime.datetime.fromtimestamp(airdate).strftime('%a %b %d, %Y')
-# 		dir.Append(Function(VideoItem(VideoPlayer, title=title, subtitle=subtitle, summary=summary, thumb=thumb, duration=duration), pid=pid, show=show, title=title, id=id))
-# 
-# 	return dir
-
 ####################################################################################################
 
 def SeasonsPage(cats, network, showtitle):
@@ -185,23 +138,15 @@ def SeasonsPage(cats, network, showtitle):
 	oc = ObjectContainer()
 	
 	pageURL = FEED_LIST % (network[0], network[1])
-#	Log("SeasonsPage URL: %s",pageURL)
 	content = JSON.ObjectFromURL(pageURL)
 	season_list = []
 	
 	for item in content['items']:
 		if WantedCats(item['parent'], cats) and showtitle in item['fullTitle']:
-			#Log(item)
 			title = item['fullTitle'].split('/')[3]
 			if title not in season_list:
-				if title=="":
-					# bad data from provider, this is a corner case and happens often
-					# enough that it's worth adding these in as uncategorized if they
-					# made it to the Seasons list (it means they have child elements to view)
-					title="Uncategorized Items"
 				season_list.append(title)
 				iid = item['ID']
-				#pid = item['PID']
 				thumb_url = item['thumbnailURL']
 				oc.add(
 					DirectoryObject(
@@ -213,25 +158,7 @@ def SeasonsPage(cats, network, showtitle):
 	oc.objects.sort(key = lambda obj: obj.title)
 	return oc
 
-# def SeasonsPage(sender, network):
-# 	dir = MediaContainer(title2=sender.itemTitle, viewGroup="List", art=sender.art)
-# 
-# 	content = JSON.ObjectFromURL(FEED_LIST % (network[0], network[1]))
-# 
-# 	for item in content['items']:
-# 		if sender.itemTitle in item['fullTitle']:
-# 			title = item['fullTitle']
-# 			#Log(title)
-# 			title = title.split('/')[-1]
-# 			#if title == 'New This Week':
-# 			#	title = item['title']
-# 			id = item['ID']
-# 			#thumb = item['thumbnailURL']
-# 			dir.Append(Function(DirectoryItem(VideosPage, title, thumb=sender.thumb), pid=network[0], id=id, network=network, show=title))
-# 
-# 	dir.Sort('title')
-# 
-# 	return dir
+
 
 ####################################################################################################
 def WantedCats(thisShow,cats):
@@ -241,42 +168,4 @@ def WantedCats(thisShow,cats):
 			return 1				
 	return 0
 
-####################################################################################################
-def VideoParse(pid, show, title, iid):
-
-	videosmil = HTTP.Request(DIRECT_FEED % pid).content
-	player = videosmil.split("ref src")
-	player = player[2].split('"')
-	Log("Gerk: player: %s",player[1])
-	# check for rtmpe, if it is for now use the webvideo
-	if player[1][0:8] == "rtmpe://":
-		Log("Gerk: is rtmpe based")
-		network = "showcase.ca"
-		show = show.replace(' ', '')
-		title = String.Quote(title, usePlus=True)
-		video_url = VIDEO_URL % (network, show, title, iid, show)
-		Log("Gerl: video_url: %s",video_url)
-		return Redirect(WebVideoItem(video_url))	
-
-	Log("Gerk: is NOT rtmpe based, %s", player[1][0:8])
-	if ".mp4" in player[1]:
-		player = player[1].replace(".mp4", "")
-		try:
-			clip = player.split(";")
-			clip = "mp4:" + clip[4]
-		except:
-			clip = player.split("/video/")
-			player = player.split("/video/")[0]
-			clip = "mp4:/video/" + clip[-1]
-	else:
-		player = player[1].replace(".flv", "")
-		try:
-			clip = player.split(";")
-			clip = clip[4]
-		except:
-			clip = player.split("/video/")
-			player = player.split("/video/")[0]
-			clip = "/video/" + clip[-1]
-
-	return Redirect(RTMPVideoItem(player, clip))
 
